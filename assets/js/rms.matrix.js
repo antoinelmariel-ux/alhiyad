20fbb809912f88c9814585b4580aeaf8e3e63f87
antoinelmariel-ux <antoinel.mariel@gmail.com>
2026-05-31 19:08:26 +0200

    Translate 'Probability' to 'Probabilité' in matrix

--- assets/js/rms.matrix.js
+++ assets/js/rms.matrix.js
@@ -581,7 +581,7 @@ function updateMatrixDescription(prob, impact, state = activeRiskEditState) {
     container.innerHTML = `
         <div class="matrix-description-header">${stateConfig.label}</div>
         <div class="matrix-description-section matrix-description-highlight">
-            <h4>Probability ${prob} - ${probability.label}</h4>
+            <h4>Probabilité ${prob} - ${probability.label}</h4>
             <p>${probability.text}</p>
         </div>
         <div class="matrix-description-section matrix-description-highlight">
